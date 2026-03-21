6b8eb18f73d9df53d919f7ac28708bb5fc334790
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2026-03-21 09:47:56 +0530

    Add files via upload

---
+++ quick_sort.c
@@ -0,0 +1,85 @@
+/*
+
+                P
+1 4 3 2 5 7 9 8 6
+0 1 2 3 4 5 6 7 8
+        j       
+    
+    
+
+
+
+arr[i]<arr[P]   2< 5  swap i and j valuesi++ j++
+
+i++
+
+after loop swap j and p
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+int partition(int *arr,int L,int H)
+{
+	int i,j,p;
+	j=L,p=H;
+	int temp;
+	for(i=L;i<p;i++)
+	{
+		if(arr[i]<arr[p])
+		{
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+			j++;
+		}
+	}
+	temp=arr[p];
+	arr[p]=arr[j];
+	arr[j]=temp;
+	return j;
+}
+void quickSort(int *arr,int L,int H)
+{
+	if(L<H)
+	{
+		int p=partition(arr,L,H);// j
+		quickSort(arr,L,p-1);// 
+		quickSort(arr,p+1,H);
+	}
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	quickSort(arr,0,n-1);// fun call  L H
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
+
+
+
+
+
